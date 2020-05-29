e74e61ea6ce91de3187a15d982def4c071baf2e9
kjanda-nw <kimberlyjanda2021@u.northwestern.edu>
2020-05-28 23:04:59 -0400

    extra stuff

--- MobileApp/test.R
+++ MobileApp/test.R
@@ -1,10 +1,19 @@
-preds <- read.csv("images.csv")
-images <- c('<img src="./www/test_0.jpg" height="52"></img>','<img src="./www/test_1.jpg" height="52"></img>',
-            '<img src="./www/test_2.jpg" height="52"></img>','<img src="./www/test_3.jpg" height="52"></img>',
-            '<img src="./www/test_4.jpg" height="52"></img>','<img src="./www/test_5.jpg" height="52"></img>',
-            '<img src="./www/test_6.jpg" height="52"></img>','<img src="./www/test_7.jpg" height="52"></img>',
-            '<img src="./www/test_8.jpg" height="52"></img>','<img src="./www/test_9.jpg" height="52"></img>',
-            '<img src="./www/test_10.jpg" height="52"></img>')
-tt <- cbind(preds,images)
 
 ind <- read.csv("Combined_1991_2021_Adj.csv", stringsAsFactors = FALSE)
+ind %>% 
+  filter(yr >= 2013,
+         yr <= 2016,
+         countryname %in% "Brazil"
+  ) %>% subset(select=c('yr','countryname',"forest_change","gdp"))
+
+
+ftd <- melt(ind, id.vars = c("yr","countryname","Sub.Regions"))
+
+ftd %>% 
+  filter(yr >= 2013,
+         yr <= 2021,
+         countryname %in% "Brazil",
+         variable == 'gdp'
+  )
+countries <- ind[,names(ind) %in% c("countryname","Sub.Regions")]
+countries2 <- unique(countries)
