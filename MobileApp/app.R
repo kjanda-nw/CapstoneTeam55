8edb60d11ad2b24ad25560bad70deb992d73774e
kjanda-nw <kimberlyjanda2021@u.northwestern.edu>
2020-05-30 16:53:07 -0400

    Final tweaks to app

--- MobileApp/app.R
+++ MobileApp/app.R
@@ -9,6 +9,8 @@ Purpose: Evaluate R-Shiny for creating mobile app. Walking through the tutorial 
 TUTORIAL: https://deanattali.com/blog/building-shiny-apps-tutorial/
 
 '
+options(rsconnect.max.bundle.files = 21000)
+
 #import libraries
 library(shiny)
 library(DT)
@@ -20,14 +22,19 @@ library(reshape2)
 #load the data
 ind <- read.csv("Combined_1991_2021_Adj.csv", stringsAsFactors = FALSE)
 
-#start with a simple dataset
+#Create a melted version to feed the plots
 ftd <- melt(ind, id.vars = c("yr","countryname","Sub.Regions","IncomeGroup","Country.Code"))
 
-#build a datatable with the images
+#read in csv with image id/probabilities/country/location(URL)
 is <- read.csv("images.csv")
 
-tt <- is[, names(is) %in% c("Image_Number","Image","pr_slash_burn","pr_selective_logging","CountryName","Sub.Regions")]
+#create a flag
+is$Deforestation_flag <- ifelse(is$pr_selective_logging>0.5 | is$pr_slash_burn>0.5,1,0)
+#keep only the probabilities of interest
+tt <- is[, names(is) %in% c("Image_Number","Image","CountryName","Deforestation_flag","pr_selective_logging","pr_slash_burn")]
+tt <- tt[, c("Image_Number","Image","CountryName","Deforestation_flag","pr_selective_logging","pr_slash_burn")]
 
+#set up lists for drop down selection - we want both a person readable label and the actual variable name
 vars <- c("GDP"='gdp',"Prior Year Population"='lag_pop',"Prior Year Burned Area"='lag_x_6798_7246',
           "Prior Year Soybean Oil Export"='lag_x_237_5910',"Prior Year Palm Oil Export"='lag_x_257_5910',
           "Prior Year Production of Pulpwood"='lag_x_1603_5516',
@@ -39,28 +46,43 @@ fvars <- c("Forest Area (sq. km)"='forest_area',"Prior Year Forest Area (sq. km)
            "Percent Change in Forest Area"='delta_forest_area',"Change in Forest Area (sq. km)"='forest_change',
            "Absolute Change in Forest Area (sq. km)"='abs_forest_change')
 
+#Get a unique list of countries and subregion for dropdown
 countries <- ind[,names(ind) %in% c("countryname","Sub.Regions")]
 countries2 <- unique(countries)
 
 #ui setup
-ui <- fluidPage(
+ui <- shinyUI(fluidPage(
+  #formatting for sidebar (selection pane)
+  tags$head(tags$style(
+    HTML('
+         #sidebar {
+            background-color: #E0E0E0;
+        }
+
+        body, label, input, button, select { 
+          font-family: "Arial";
+        }')
+  )),
+  #app title
   titlePanel("Forest Tracker"),
-  "Team 55",
+  #subtxt
+  "Capstone Team 55",
+  br(), #spaces
   br(),
-  br(),
-  sidebarLayout(
-    sidebarPanel(
-    sliderInput("yrInput","Year",1991,2021,value=c(2011,2021),sep=""), #update with predictions
-    selectizeInput("fplot_var", "Forest Variable", choices = fvars, selected="forest_area"),
-    selectizeInput("plot_var", "Predictor Variable",choices = vars),
+  sidebarLayout( #set up user select panel
+    sidebarPanel(id="sidebar",
+    sliderInput("yrInput","Year",1991,2021,value=c(2011,2021),sep=""), #slider defaults to most recent 5 years + predictions
+    selectizeInput("fplot_var", "Forest Variable", choices = fvars, selected="forest_area"), #users can select a forest var
+    selectizeInput("plot_var", "Predictor Variable",choices = vars), #users can select a model feature
     selectInput("countryInput","Country",
                 choice=split(countries2$countryname,countries2$Sub.Regions),
-                selected = "Brazil", multiple = TRUE)
+                selected = "Brazil", multiple = TRUE) #users can select one or more countries (defaults to Brazil)
     ),
+    #main tabs
     mainPanel(
       tabsetPanel(type="tabs",
                   tabPanel("Images",DT::dataTableOutput("images")),
-                  tabPanel("Plot",
+                  tabPanel("Plots",
                            plotOutput("areaplot"),
                            plotOutput("predplot")
                   ),
@@ -68,15 +90,11 @@ ui <- fluidPage(
       )
     )
   )
-)
+))
 #server setup
-server <- function(input, output) {
-  #output$countryOutput <- renderUI({
-  #  selectInput("countryInput","Country",
-  #              choice=split(countries2$Sub.Regions,countries2$countryname),
-  #              selected = "Brazil", multiple = TRUE)
-  #})
-  
+server <- shinyServer(function(input, output) {
+
+  #filter dataset for table (reactive to user inputs)
   filtered <- reactive ({
     if (is.null(input$countryInput)) {
       return(NULL)
@@ -89,6 +107,7 @@ server <- function(input, output) {
       ) %>% subset(select=c('yr','countryname',input$fplot_var,input$plot_var))
     
   })
+  #filter dataset for predictor plot (reactive to user inputs)
   plotdata<- reactive ({
     if (is.null(input$countryInput)) {
       return(NULL)
@@ -101,6 +120,7 @@ server <- function(input, output) {
              variable == var
       )
   })
+  #filter dataset for forest plot (reactive to user inputs)
   fplotdata <- reactive ({
     if (is.null(input$countryInput)) {
       return(NULL)
@@ -114,7 +134,7 @@ server <- function(input, output) {
       )
     
   })
-  
+  #filter dataset of images (reactive to user inputs)
   ifiltered <- reactive ({
     if (is.null(input$countryInput)) {
       return(NULL)
@@ -122,36 +142,39 @@ server <- function(input, output) {
     
     tt %>%
       filter(CountryName %in% input$countryInput) %>%
-      arrange(desc(pr_selective_logging))
+      arrange(desc(Deforestation_flag))
   })
-  
+  #plot forest variable
   output$areaplot <- renderPlot({
     if (is.null(fplotdata())) {
       return()
     }
     ggplot(fplotdata()) +
       geom_line(mapping = aes(x=yr, y=value, colour=countryname)) +
-      labs(x="Year", title=input$fplot_var) +
+      labs(x="Year", y="", title=input$fplot_var) +
       scale_colour_discrete(name="Country")
-  })
+  },height=300
+  )
+  #plot predictor variable
   output$predplot <- renderPlot({
     if (is.null(plotdata())) {
       return()
     }
     ggplot(plotdata(), aes(x=yr, y=value, colour=countryname)) +
       geom_line() +
-      labs(x="Year", title=input$plot_var) +
+      labs(x="Year", y="", title=input$plot_var) +
       scale_colour_discrete(name="Country")
-  })
-  
+  },height=300
+  )
+  #render table 
   output$results <- renderTable({
     filtered()
   })
-  
+  #render image table
   output$images <- DT::renderDataTable({
     DT::datatable(ifiltered(),escape = FALSE)
   })
-}
+})
 
 #initial app
 shinyApp(ui = ui, server = server)
